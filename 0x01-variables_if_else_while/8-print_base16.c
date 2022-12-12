69c3083138509209f846acaa41a63a5a4f208781
Lizzy-dev27 <chaukee555@gmail.com>
2022-12-12 15:16:50 -0800

    Print base

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * mian - print all the numbers of base 16 in lowercase
+ *
+ * Return: 0 (S uccess)
+ */
+
+int main(void)
+{
+	int num = '0'; /*Initialize num to 0*/
+	char letter = 'a'; /*Initialize letter to a*/
+
+	while (num <= '9') /*Print 0-9*/
+	{
+		putchar(num);
+		num++;
+	}
+	while (letter <= 'f') /*Print a-f*/
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+

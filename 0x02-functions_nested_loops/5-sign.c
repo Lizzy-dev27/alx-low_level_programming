71216956208ddacb3adf4860068648d37d17d21d
Lizzy-dev27 <chaukee555@gmail.com>
2022-12-13 09:16:02 -0800

    6-abs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - return 0 letter not lowercase, 1 letter lowercase
- * 
+ *
  * @n : number to check
  * Return: 0 or 1
  *
@@ -13,19 +13,20 @@ int print_sign(int n)
 	int test;
 
 	if (n > 0)
-        {
+	{
 		test = 1;
 		_putchar('+');
 	}
-        else if (n == 0)
-        {
+	else if (n == 0)
+	{
 		test = 0;
 		_putchar('0');
 	}
-        else
-        {
+	else
+	{
 		test = -1;
 		_putchar('-');
 	}
-        return (test);
+	return (test);
 }
+		

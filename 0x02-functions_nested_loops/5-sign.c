ad64ca820cac0cada2f6c1826111333b63341a91
Lizzy-dev27 <chaukee555@gmail.com>
2022-12-13 08:36:49 -0800

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,23 +9,30 @@
  */
 
 int print_sign(int n)
+
 {
 	int test;
 
 	if (n > 0)
+
 	{
 		test = 1;
 		_putchar('+');
 	}
+
 	else if (n == 0)
+
 	{
 		test = 0;
 		_putchar('0');
 	}
+
 	else
+
 	{
 		test = -1;
 		_putchar('-');
 	}
+
 	return (test);
 }

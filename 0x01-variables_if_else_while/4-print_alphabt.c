961dfee54ba7d9d628f1806a89fdd9c0705e53dc
Lizzy-dev27 <chaukee555@gmail.com>
2022-12-14 08:15:15 -0800

    4-print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,18 +8,18 @@
  */
 
 int main(void)
+
 {
 	char ch = 'a';
 
 	while (ch <= 'z')
 	{
-		if ((ch != 'e') && (ch != 'q'));
+		if ((ch != 'e') && (ch != 'q'))
 		{
 			putchar(ch);
 		}
 		ch++;
 	}
 	putchar('\n');
-        return (0);
+	return (0);
 }
-

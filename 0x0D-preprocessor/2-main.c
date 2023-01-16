f0aa77d44af8ce095479ee3f91b6c6d87d645352
Lizzy-dev27 <chaukee555@gmail.com>
2023-01-16 11:04:45 -0800

    Main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,8 @@
-#include <instdio.h>
-
+#include <stdio.h>
 /**
- * main - Program compilation starts from the main
- * Description: this program prints the name of the file it was comipiled from
- * Return: 0 (success
+ * main - print the name of the file
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {

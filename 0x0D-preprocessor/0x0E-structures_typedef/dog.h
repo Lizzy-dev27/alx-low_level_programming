09eae541896373e39ff40ee4bdaa67c14289850a
Lizzy-dev27 <chaukee555@gmail.com>
2023-01-16 12:57:11 -0800

    dog

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -1,11 +1,10 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - user defined data type
- * @name: First member
- * @age: Second member
- * @owner: Third member
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 struct dog
 {
@@ -18,7 +17,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
